ae0d899ed118da0138fdc4b8bb3ca739b6d90475
AdamK <44851323+adamski10@users.noreply.github.com>
2020-07-21 19:15:42 +0200

    mvp

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class Library {
+    private int capacity;
+    private ArrayList<Book> bookStock;
+
+    public Library(int capacity){
+        this.capacity = capacity;
+        this.bookStock = new ArrayList<Book>();
+    }
+
+    public int getStockCount(){
+        return this.bookStock.size();
+    }
+
+    public String addBook(Book book){
+        if(getStockCount() < this.capacity){
+            this.bookStock.add(book);
+            return "Book added successfully";
+        }else
+            return "Stock full";
+    }
+
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,31 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+    private Library library;
+    private Book book1;
+    private Book book2;
+    private Book book3;
+
+    @Before
+    public void before() {
+        library = new Library(2);
+    }
+
+    @Test
+    public void canAddBook() {
+        library.addBook(book1);
+        assertEquals(1, library.getStockCount());
+    }
+
+    @Test
+    public void canNotAddBookIfStockFull(){
+        library.addBook(book1);
+        library.addBook(book2);
+        assertEquals("Stock full", library.addBook(book3));
+    }
+
 }
